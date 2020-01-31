84251607729d5c9f4130e2b1f4aa787164eeb6d4
Herael <b.rousval@hotmail.com>
2020-01-31 22:24:58 +0100

    init fastlane and CI (With Alpheonix in da place)

--- StarFighter/StarFighter/Platforms/Mock/SpaceshipMockService.swift
+++ StarFighter/StarFighter/Platforms/Mock/SpaceshipMockService.swift
@@ -13,7 +13,7 @@ class SpaceshipMockService: SpaceshipService {
     private let spaceships: [Spaceship] = [
         Spaceship(id: "1", name: "Executor", model: "Executor-class star dreadnought", manufacturer: "Kuat Drive Yards, Fondor Shipyards", speed: 40, hp: 600000, damage: 100000, spaceshipClass: "Star dreadnought" ,img: URL(string: "https://vignette.wikia.nocookie.net/spore/images/4/46/Executor-class_Star_Dreadnought.png/revision/latest/scale-to-width-down/1000?cb=20161018175227")),
         
-        Spaceship(id: "2", name: "Sentinel-class landing craft", model: "Sentinel-class landing craft", manufacturer: "Sienar Fleet Systems, Cyngus Spaceworks", speed: 70, hp: 54000, damage: 8000, spaceshipClass: "landing craft", img: URL(string: "https://vignette.wikia.nocookie.net/starwars/images/6/6e/Sentinel_negvv.png/revision/latest?cb=20170411232732"))
+        Spaceship(id: "2", name: "Sentinel-class landing craft", model: "Sentinel-class landing craft", manufacturer: "Sienar Fleet Systems, Cyngus Spaceworks", speed: 110, hp: 54000, damage: 8000, spaceshipClass: "landing craft", img: URL(string: "https://vignette.wikia.nocookie.net/starwars/images/6/6e/Sentinel_negvv.png/revision/latest?cb=20170411232732"))
     ]
     
     func create(name: String, completion: @escaping (Bool) -> Void) {
