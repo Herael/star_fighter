4596832b04040375d82383409f8276015ac90531
Fabiana <fabimontiel@gmail.com>
2020-02-04 21:38:47 +0100

    game center sign up and leaderboard

--- StarFighter/Scenes/Home/HomeViewController.swift
+++ StarFighter/Scenes/Home/HomeViewController.swift
@@ -7,27 +7,105 @@
 //
 
 import UIKit
+import GameKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, GKGameCenterControllerDelegate {
+    
+    @IBOutlet var label_score: UILabel!
+    
+    /* Variables */
+    var gcEnabled = Bool() // Check if the user has Game Center enabled
+    var gcDefaultLeaderBoard = String() // Check the default leaderboardID
+         
+    var score = 0
+         
+    // IMPORTANT: replace the red string below with your own Leaderboard ID (the one you've set in iTunes Connect)
+    let LEADERBOARD_ID = "com.esgi.findout"
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Show initial score
+        label_score.text = "\(score)"
+        
+        // Call the GC authentication controller
+        authenticateLocalPlayer()
+        
         // Do any additional setup after loading the view.
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    
+    func authenticateLocalPlayer() {
+        let localPlayer = GKLocalPlayer()// = GKLocalPlayer.localPlayer()
+        
+        localPlayer.authenticateHandler = {(ViewController, error) -> Void in
+            if((ViewController) != nil) {
+                // 1 Show login if player is not logged in
+                self.present(ViewController!, animated: true, completion: nil)
+            } else if (localPlayer.isAuthenticated) {
+                // 2 Player is already euthenticated & logged in, load game center
+                self.gcEnabled = true
+                    
+                // Get the default leaderboard ID
+                localPlayer.loadDefaultLeaderboardIdentifier(completionHandler: { (leaderboardIdentifer, error) in
+                    if error != nil { print(error)
+                    } else { self.gcDefaultLeaderBoard = leaderboardIdentifer! }
+                })
+                
+            } else {
+                // 3 Game center is not enabled on the users device
+                self.gcEnabled = false
+                print("Local player could not be authenticated!")
+                print(error)
+            }
+        }
     }
-    */
+    
+    @IBAction func addScoreSubmit(_ sender: Any) {
+        
+        // Add 10 points to current score
+        score += 10
+        label_score.text = "\(score)"
+        
+        // Submit score to GC leaderboard
+        let bestScoreInt = GKScore(leaderboardIdentifier: LEADERBOARD_ID)
+        bestScoreInt.value = Int64(score)
+        GKScore.report([bestScoreInt]) { (error) in
+            if error != nil {
+                print(error!.localizedDescription)
+            } else {
+                print("Best Score submitted to your Leaderboard!")
+            }
+        }
+        
+    }
+    
+    // Delegate to dismiss the GC controller
+    func gameCenterViewControllerDidFinish(_ gameCenterViewController: GKGameCenterViewController) {
+        gameCenterViewController.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func checkLeaderboard(_ sender: Any) {
+        
+        let gcVC = GKGameCenterViewController()
+        gcVC.gameCenterDelegate = self
+        gcVC.viewState = .leaderboards
+        gcVC.leaderboardIdentifier = LEADERBOARD_ID
+        present(gcVC, animated: true, completion: nil)
+        
+    }
+    
     @IBAction func touchButton(_ sender: Any) {
         let spaceshipList = StarshipTableViewController()
         self.navigationController?.pushViewController(spaceshipList, animated: true)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        
     }
     
 }
